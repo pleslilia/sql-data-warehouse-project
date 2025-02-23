6acc79671282fb6319f7649d39c932181e66c0f0
Lilia <167005278+pleslilia@users.noreply.github.com>
2025-02-23 20:16:07 +0100

    Create clean_transform_data.sql

---
+++ scripts/silver/clean_transform_data.sql
@@ -0,0 +1,176 @@
+-- Quality Check for BRONZE LAYER
+-- Check for Nulls or Duplicates in Primary Key
+-- Expectation: No Result
+
+SELECT 
+cst_id,
+COUNT(*)
+FROM bronze.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) > 1 OR cst_id IS NULL;
+
+-- Problem: we detected the duplicates in the column cst_id, and also NULLS we will proceed one by one using the duplicate id (ex. WHERE cst_id = 29466)
+
+-- Solution:
+-- We will RANK all duplicated values based on cst_create_date and hoose only the highest one
+-- Use ROW_NUMBER() - assigns a unique number to each row in a result set, based on a defined order
+
+SELECT 
+*,
+ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
+FROM bronze.crm_cust_info
+WHERE cst_id = 29466;
+
+
+
+SELECT 
+*,
+ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
+FROM bronze.crm_cust_info;
+
+
+
+SELECT * FROM (
+SELECT 
+*,
+ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
+FROM bronze.crm_cust_info)t WHERE flag_last != 1;
+
+
+SELECT * FROM (
+SELECT 
+*,
+ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
+FROM bronze.crm_cust_info)t WHERE flag_last = 1 AND cst_id = 29466;
+
+
+-- Quality check
+-- Check for unwanted spaces in string values
+-- We will use TRIM() function - removes leading and trailing spaces from a string
+-- If the original value is not equal to the same value after trimming, it means there are spaces!
+SELECT cst_firstname
+FROM bronze.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname)
+
+
+SELECT cst_lastname
+FROM bronze.crm_cust_info
+WHERE cst_lastname != TRIM(cst_lastname)
+
+
+SELECT cst_gndr
+FROM bronze.crm_cust_info
+WHERE cst_gndr != TRIM(cst_gndr)
+
+
+SELECT
+cst_id,
+cst_key,
+TRIM(cst_firstname) AS cst_firstname,
+TRIM(cst_lastname) AS cst_lastname,
+cst_marital_status,
+cst_gndr,
+cst_create_date
+FROM (
+	SELECT *,
+	ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
+	FROM bronze.crm_cust_info
+	WHERE cst_id IS NOT NULL
+)t WHERE flag_last = 1
+
+
+-- Quality check: Check the consistency of values in low cardinality columns (cst_marital_status, cst_gndr)
+-- Low-cardinality refers to columns with few unique values
+
+--Find uniques possible values:
+SELECT DISTINCT cst_gndr
+FROM bronze.crm_cust_info
+
+-- In our data warehouse, we aim to store clear and meaningful values rather than using abbreviated terms
+-- So we will do the rule for the whole project, instead of having M --> Male, instead of F --> Female
+-- Apply UPPER() just in case mixed-case values appear later in out column cst_gndr
+-- Apply TRIM() just in case spaces appear later in your column
+-- And in our data warehouse, we use the default calue "n/a" for missing values (for NUULs)
+
+SELECT
+cst_id,
+cst_key,
+TRIM(cst_firstname) AS cst_firstname,
+TRIM(cst_lastname) AS cst_lastname,
+CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+	 WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+	 ELSE 'n/a'
+END cst_marital_status,
+CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+	 WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+	 ELSE 'n/a'
+END cst_gndr,
+cst_create_date
+FROM (
+	SELECT *,
+	ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
+	FROM bronze.crm_cust_info
+	WHERE cst_id IS NOT NULL
+)t WHERE flag_last = 1
+
+
+-- Write INSERT statement
+INSERT INTO silver.crm_cust_info (
+	cst_id,
+    cst_key,
+    cst_firstname,
+    cst_lastname,
+    cst_marital_status,
+    cst_gndr,
+    cst_create_date)
+
+SELECT
+cst_id,
+cst_key,
+TRIM(cst_firstname) AS cst_firstname,
+TRIM(cst_lastname) AS cst_lastname,
+CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+	 WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+	 ELSE 'n/a'
+END cst_marital_status,
+CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+	 WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+	 ELSE 'n/a'
+END cst_gndr,
+cst_create_date
+FROM (
+	SELECT *,
+	ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
+	FROM bronze.crm_cust_info
+	WHERE cst_id IS NOT NULL
+)t WHERE flag_last = 1
+
+SELECT TOP 1000 * FROM silver.crm_cust_info;
+
+
+-- Once the cleaned data was inserted into SILVER LAYER --> RE-DO the Data Quality Checks: 
+SELECT 
+cst_id,
+COUNT(*)
+FROM silver.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) > 1 OR cst_id IS NULL;
+
+
+SELECT cst_firstname
+FROM silver.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname)
+
+
+SELECT cst_lastname
+FROM silver.crm_cust_info
+WHERE cst_lastname != TRIM(cst_lastname)
+
+
+SELECT cst_gndr
+FROM silver.crm_cust_info
+WHERE cst_gndr != TRIM(cst_gndr)
+
+  
+SELECT DISTINCT cst_gndr
+FROM silver.crm_cust_info
